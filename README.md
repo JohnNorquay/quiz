1fe09a9264b09d06605ff9fab179fde7872ea082
John Norquay <John.Norquay@gmail.com>
2019-01-25 17:07:38 -0600

    Completed quiz with timer and channels

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # quiz
+This quiz needs a csv file within the directory of the program.  This csv file
+should be formatted question, answer.  for example:
+
+8 + 6, 14
+
+The default name of the file is problems.csv, but can be changed on the
+command line via the flag --csvFile=  .  For example:
+
+go run main.go --csvFile=test.csv
+
+This program has a timer you can set that determines how much time (in seconds)
+are given to complete the quiz.  The default time limit is 30 seconds.  This
+can also be changed via the command line flag of --time= . For example:
+
+go run main.go --time=60
+
+This quiz will tally the number of correct answers given.  Once the time limit
+is up, the test will stop.  The number of correct answers will be given as
+well as the total amount of questions on the quiz.  
+
+If the student is in the middle of answering a question and time is up, the
+test will stop, not allowing the student to finish the question.
+
+Have fun formulating your own tests!
--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
+// Problem struct allows us to give structure to our quiz.  By utilizing This
+// approach we can have different types of input and still have the tests
+// be functional
 type Problem struct {
 	Question string
 	Answer   string
 }
 
 func main() {
-	csv1 := flag.String("csvFile", "problems.csv", "file for Questions format: question, answer")
+	csv1 := flag.String("csvFile", "problems.csv", "file for Questions format: question, answer.")
+	timeLimit := flag.Int("time", 5, "Define the time limit for the quiz.")
 	flag.Parse()
 
 	csvFile, err := os.Open(*csv1)
@@ -24,6 +29,58 @@ func main() {
 	lines, err := csv.NewReader(csvFile).ReadAll()
 	Check(err)
 
+	problems := ParseLines(lines)
+
+	fmt.Println("Press <enter> when ready to begin quiz:")
+	fmt.Scanln()
+	fmt.Println("Good Luck!!\n\n")
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	numCorrect := 0
+	ansChan := make(chan string)
+
+	// problemloop is like a goto statement.  When the timer is up, the program
+	// breaks back to problemloop and then finishes by printing results of the quiz.
+problemloop:
+
+	// we are running through each question that is already read into memory.
+	// we are utilizing an anonymous function as a go routine.  This will run
+	// in the background and keep track of how much time is left for completion
+	// of the quiz.
+	for i, line := range problems {
+		fmt.Printf("Question %d: %s  ", i+1, line.Question) // question is asked.
+		go func() {
+			var answer string
+			fmt.Scan(&answer) // waits for an answer to be input
+			ansChan <- answer // the input answer is placed into a channel.
+		}()
+
+		// The select statement is the key to this program finishing as soon as the
+		// timer is done. . .
+		select {
+		case <-timer.C: // if time is up, there is something in this channel.
+			fmt.Println("Times Up!")
+			break problemloop // if something is in the channel, we quit.
+		case answer := <-ansChan: // if nothing is in the timer channel,
+			// then we pull the answer waiting in the answer
+			// channel.
+			if answer == line.Answer {
+				numCorrect++
+				fmt.Print("Correct\n")
+			} else {
+				fmt.Println("Wrong")
+			}
+		}
+	}
+
+	fmt.Printf("\n\nYou got %d out of %d correct.\n\n", numCorrect, len(problems))
+}
+
+// ParseLines reads through a file already read into memory.  It takes each
+// question and answer from a single line and populates the Problems struct.
+// A slice of Problems are then created and returned.
+func ParseLines(lines [][]string) []Problem {
 	Problems := make([]Problem, len(lines))
 	for i, line := range lines {
 		Problems[i] = Problem{
@@ -31,24 +88,10 @@ func main() {
 			Answer:   strings.TrimSpace(line[1]),
 		}
 	}
-
-	numCorrect := 0
-	var answer string
-	for i, line := range Problems {
-		fmt.Printf("Question %d: %s  ", i+1, line.Question)
-		fmt.Scan(&answer)
-
-		if answer == line.Answer {
-			numCorrect++
-			fmt.Print("Correct\n")
-		} else {
-			fmt.Println("Wrong")
-		}
-	}
-
-	fmt.Printf("\n\nYou got %d out of %d correct.\n\n", numCorrect, len(Problems))
+	return Problems
 }
 
+// Check() checks for an error.  If present, returns the error and then panics.
 func Check(err error) {
 	if err != nil {
 		fmt.Println("Error:", err)
